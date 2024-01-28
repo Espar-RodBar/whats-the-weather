171e0d50d2d0ef0c1cde7fa11419d481b89b7539
Espar-RodBar <cr4wl3r@gmail.com>
2024-01-28 13:34:12 +0100

    removed const API

--- scripts/model.js
+++ scripts/model.js
@@ -6,8 +6,6 @@ import {
   MAX_DAYS,
 } from './config.js'
 
-const API = process.env.GEOCODE_API ? `&auth=${process.env.GEOCODE_API}` : ''
-
 import { AJAX } from './helpers.js'
 
 export const state = {
@@ -121,7 +119,7 @@ class DayWeatherInfo {
 
 export async function getWeatherData() {
   try {
-    const fetchURL = `${METEO_API_URL}latitude=${state.lat}&longitude=${state.lon}${METEO_API_OPT}${API}`
+    const fetchURL = `${METEO_API_URL}latitude=${state.lat}&longitude=${state.lon}${METEO_API_OPT}`
     console.log(fetchURL)
     const data = await AJAX(fetchURL)
 
