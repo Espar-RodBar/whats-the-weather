1b6c57b533aeaaf59a27dcd8b952f03608c3ae7b
Espar-RodBar <cr4wl3r@gmail.com>
2024-01-28 14:19:08 +0100

    fixed netlify function

--- netlify/functions/getPosFromCity.mjs
+++ netlify/functions/getPosFromCity.mjs
@@ -2,19 +2,20 @@ export default async (request, context) => {
   const apiKey = Netlify.env.get('GEOCODE_API')
   const { city } = context.params
   const GEOPARSING_API_URL = `https://geocode.xyz/?${city}`
+
   try {
     const response = await fetch(`${GEOPARSING_API_URL}&auth=${apiKey}`)
     const data = await response.json()
 
-    return {
+    return new Response({
       statusCode: 200,
       body: data,
-    }
+    })
   } catch (err) {
-    return {
+    return new Response({
       statusCode: 400,
       body: { message: err },
-    }
+    })
   }
 }
 
